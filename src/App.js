517acf7308bf216c1b52d404842cf51142e51085
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-01-07 02:12:05 -0800

    Updated State

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { IncomeExpenses } from './IncomeExpenses';
 import { TransactionList } from './TransactionList';
 import { AddTransaction } from './AddTransaction';
 
-import { GlobalProvider } from 'context/GlobalState';
+import { GlobalProvider } from './GlobalState';
 
 import './App.css';
 
